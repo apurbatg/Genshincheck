6c19fd1d71645eb46d976be5edaa439716a58a8e
Apurba <93421564+apurbatg@users.noreply.github.com>
2023-05-02 16:05:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,94 +1,58 @@
-import os
+import telegram
 
-import logging
+from telegram.ext import Updater, CommandHandler
 
-from genshinstats import client as gsclient
+from genshinstats import GenshinStats
 
-from genshinstats.errors import GenshinStatsError
+from genshinstats.errors import InvalidUID
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+TOKEN = '6270072880:AAFFspd4Blqglk__gGyHEJS1BdMml_dxr2M'
 
-# Set up logging
+bot = telegram.Bot(token=TOKEN)
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-
-                    level=logging.INFO)
-
-# Define the command handlers
+gs = GenshinStats()
 
 def start(update, context):
 
-    """Send a welcome message when the command /start is issued."""
+    """Send a message when the command /start is issued."""
 
-    update.message.reply_text('Hi! I am a Genshin Impact stats bot. Send me a player UID to get their stats!')
+    update.message.reply_text('Welcome to the Genshin Impact Stats bot!')
 
-def help(update, context):
+def search(update, context):
 
-    """Send a help message when the command /help is issued."""
+    """Retrieve player stats when the command /search is issued."""
 
-    update.message.reply_text('Send me a player UID to get their stats!')
+    args = context.args
 
-def echo(update, context):
+    if len(args) == 0:
 
-    """Search for the player with the given UID and display their stats."""
+        update.message.reply_text('Please enter a player UID.')
 
-    uid = update.message.text
+    else:
 
-    try:
+        uid = args[0]
 
-        player_data = gsclient.get_user_stats(uid)
+        try:
 
-        message = f"Stats for UID {uid}:\n"
+            stats = gs.get_user_stats(uid)
 
-        message += f"Adventure Rank: {player_data['stats']['level']}\n"
+            update.message.reply_text(f'Player stats for UID {uid}: {stats}')
 
-        message += f"Spiral Abyss: Floor {player_data['stats']['spiral_abyss']['floor']} - Chamber {player_data['stats']['spiral_abyss']['max_floor']} ({player_data['stats']['spiral_abyss']['total_star']}⭐)\n"
+        except InvalidUID:
 
-        update.message.reply_text(message)
-
-    except GenshinStatsError:
-
-        update.message.reply_text("Player not found. Please check the UID and try again.")
-
-def error(update, context):
-
-    """Log the error message."""
-
-    logging.error(f"Update {update} caused error {context.error}")
+            update.message.reply_text('Invalid UID. Please enter a valid UID.')
 
 def main():
 
     """Start the bot."""
 
-    # Get the Telegram bot token from the environment variable
-
-    bot_token = os.environ.get('BOT_TOKEN')
-
-    if bot_token is None:
-
-        logging.error("Please set the BOT_TOKEN environment variable.")
-
-        return
-
-    # Set up the Telegram bot updater and dispatcher
-
-    updater = Updater(bot_token, use_context=True)
+    updater = Updater(TOKEN, use_context=True)
 
     dp = updater.dispatcher
 
-    # Add the command and message handlers
-
     dp.add_handler(CommandHandler('start', start))
 
-    dp.add_handler(CommandHandler('help', help))
-
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-
-    # Log all errors
-
-    dp.add_error_handler(error)
-
-    # Start the bot
+    dp.add_handler(CommandHandler('search', search))
 
     updater.start_polling()
 
@@ -97,4 +61,3 @@ def main():
 if __name__ == '__main__':
 
     main()
-
